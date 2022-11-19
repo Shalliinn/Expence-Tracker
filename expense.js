74791af7461f57fb485e906ff63b2d2efded36ba
Shalliinn <113151132+Shalliinn@users.noreply.github.com>
2022-11-19 06:05:54 +0530

    Add files via upload

--- expense.js
+++ expense.js
@@ -1,11 +1,14 @@
 const Expenseuser=require('../models/expenseuser')
-
+const Expuser=require('../models/exp-user')
+const S3service=require('../services/s3services')
+const Userservices=require('../services/userservices')
+const files=require('../models/file')
 exports.postexpense=(req,res,next)=>{
     const expence=req.body.expence
     const description=req.body.description
     const category =req.body.category
     //.log(req.user.userId,'8');
-    const id=req.user.userId
+    const id=req.user.id
     Expenseuser.create({
         expence:expence,
         description:description,
@@ -18,15 +21,35 @@ exports.postexpense=(req,res,next)=>{
 }
 
 exports.getexpense=(req,res,next)=>{
-  //  console.log(req.user.userId,'19');
-    Expenseuser.findAll({where: {expuserId:req.user.userId}})
-    .then((exp)=>{
-   //  console.log(exp);
-        res.status(200).json({Allexp:exp})
+   console.log(req.user.name,'19');
+if(req.user.ispremiumuser){
+    Expuser.findAll({include:['expenes']})
+    .then((alluser)=>{
+       return req.user.getFiles()
+       .then((files)=>{
+        console.log(files,'30');
+        res.status(200).json({user:alluser,ispremiumuser:req.user.ispremiumuser,name:req.user.name,file:files})
+       })
+
      })
 .catch (error=>{
     console.log(error)
 })
+}
+else{
+    Expenseuser.findAll({where: {expuserId:req.user.id}})
+    .then((alluser)=>{
+        return req.user.getFiles()
+       .then((files)=>{
+        console.log(files,'30');
+ //  console.log(exp);
+        res.status(200).json({user:alluser,ispremiumuser:req.user.ispremiumuser,file:files})
+    })
+     })
+.catch (error=>{
+    console.log(error)
+})
+}
  }
  
  exports.deleteexpense=(req,res,next)=>{
@@ -36,6 +59,33 @@ exports.getexpense=(req,res,next)=>{
     res.sendStatus(200)
    })
 .catch(err=>console.log(err))
+}
+
+exports.downloadexpense= async (req,res)=>{
+    try{
+   // const expenses= await req.user.getExpenes();
+    const expenses= await Userservices.getExpenes(req)
+  //  console.log(expenses,'55');
+  const stringifiedExpenses=JSON.stringify(expenses)
+  const userId=req.user.id;
+  const filename=`Expense${userId}/${new Date}.txt`;
+ // const fileUrl=await uploadToS3(stringifiedExpenses,filename);
+  const fileUrl=await S3service.uploadToS3(stringifiedExpenses,filename);
+
+  files.create({
+    fileUrl:fileUrl,
+    expuserId:userId    
+  })
+  Expuser.findAll({include:['files']}).then((user)=>{
+    res.status(200).json({fileUrl,success:true,user:user,id:userId})
+  })
+  
+  
+    }catch{
+        console.log(err);
+        res.status(500).json({fileUrl:'',success:false,err})
+
+    }
 }
 
 
@@ -71,4 +121,4 @@ exports.postEditexpense = (req, res, next) => {
 //   };
 
 
-let product=[]
+
